bbd1e11212621ee657c035cec7f7b2adddf703d7
yarov-475 <ya.yarochkin@yandex.ru>
2020-07-03 16:52:07 +0300

    ferst

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () => {
+	return (
+		<header className="header">
+			<img src="https://author.today/content/2019/08/12/16d805b54b8e446da8718f51a6208727.jpg?width=750&height=500&mode=min"></img>
+		</header>
+
+	);
+
+}
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Navbar = () => {
+	return (
+		<nav className="nav">
+
+			<div> <a>Profile </a></div>
+			<div><a>MESAGES </a> </div>
+			<div> <a> NEWS</a></div>
+			<div><a>MUSIC</a></div>
+			<div><a>SETTINGS</a></div>
+
+		</nav>
+
+	);
+
+}
+export default Navbar;
