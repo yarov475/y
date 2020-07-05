590aa83e9c93b76844aad4eb6adbfa548d13f55e
yarov-475 <ya.yarochkin@yandex.ru>
2020-07-05 18:58:22 +0300

    favicon23lesson

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,10 @@ import s from "./Header.module.css";
 const Header = () => {
 	return (
 		<header className={s.header}>
-			<img src="https://author.today/content/2019/08/12/16d805b54b8e446da8718f51a6208727.jpg?width=750&height=500&mode=min" width="100%"></img>
+			<img src="https://author.today/content/2019/08/12/16d805b54b8e446da8718f51a6208727.jpg?width=750&height=500&mode=min" ></img>
+			{/*<img src="./../img/kite.png"	></img>*/}
+
+
 			<div className={s.item}>MY FRIENDS</div>
 		</header>
 
