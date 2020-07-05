181d212e0bc4f369ef7fcea98f03483bf7318e80
yarov-475 <ya.yarochkin@yandex.ru>
2020-07-05 18:31:56 +0300

    dialog refector

--- src/components/Dialog/Dialogs.jsx
+++ src/components/Dialog/Dialogs.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import s from  './Dialog.module.css'
+import {NavLink} from "react-router-dom";
 
+const Dialogitem = (props)=> {
+    let path = "/dialogs/"+props.id;
+
+    return <div className={s.dialog + ' ' + s.active} >
+        <NavLink to={path} >  {props.name} </NavLink>
+    </div>
+}
+const Message = (props)=>{
+    return<div className={s.dialog}>{props.message}</div>
+
+}
 
 const Dialog =(props)=>{
     return(
+
                    <div className={s.dialogs}>
                 <div className={s.dialogITems}>
-                    <div className={s.dialog + '' + s.active} >Dima</div>
-                <div className={s.dialog}>Vova</div>
-                    <div className={s.dialog}>leha</div>
-                <div className={s.dialog}>Jon</div>
-                    <div className={s.dialog}>Micle</div>
-                <div className={s.dialog}>Andru</div>
+                    <Dialogitem name ="Dmitry" id={"1"}></Dialogitem>
+                    <Dialogitem name ="Vova" id={"2"}></Dialogitem>
+                    <Dialogitem name ="Leha" id={"3"}></Dialogitem>
+                    <Dialogitem name ="Jon" id={"4"}></Dialogitem>
+
+
                 </div>
 
                     <div className={s.messages}>
-                        <div className={s.dialog}>Hi</div>
-                        <div className={s.dialog}>Love</div>
-                        <div className={s.dialog}>Hello</div>
-
+                        <Message message={"love yoy"}
+                        ></Message> <Message message={"very "}
+                        ></Message> <Message message={"much yoy"}
+                        ></Message>
                 </div>
                 </div>
     )
