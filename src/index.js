9276791493a6ad8e24f2f3f30dc89c121d18a6f7
yayar <ya.yarochkin@yandex.ru>
2020-07-17 13:08:21 +0100

    38

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import  state , {addPost, subscribe, updateNewPostText} from "./redux/state";
+import  store from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 
@@ -13,13 +13,14 @@ import './index.css';
 let rerenderEntireTree = (state)=>{
     ReactDOM.render(
         <BrowserRouter>
-            <App  state = {state} addPost={addPost}  updateNewPostText = {updateNewPostText} />
+            <App  state = {store.getState()} addPost={store.addPost.bind(store)}
+                  updateNewPostText = {store.updateNewPostText.bind(store)} />
         </BrowserRouter>,
         document.getElementById('root'));
 }
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,20 +1,15 @@
 // import React from "react";
-
-
-let rerenderEntireTree = ()=>  {
-    console.log('state changed');
-}
-
-let state = {
-    profilePage: {
-        newPostText:"Hi!",
-        posts: [
-            {id: 1, message: "how are you?", likesCount: 12},
-            {id: 2, message: "helo Frirnd!!!", likesCount: 15},
-            {id: 3, message: "helo Frirnds!!!", likesCount: 15}
-        ]
-    },
-    dialogPage: {
+let store = {
+    _state: {
+        profilePage: {
+            newPostText:"Hi!",
+            posts: [
+                {id: 1, message: "how are you?", likesCount: 12},
+                {id: 2, message: "helo Frirnd!!!", likesCount: 15},
+                {id: 3, message: "helo Frirnds!!!", likesCount: 15}
+            ]
+        },
+        dialogPage: {
             dialogs: [
                 {id: 1, name: 'Dima', src:"data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxMTEhUTExMWFhUXFxUVFxcYFxoYFxUYFRUWFxUVFxcYHSggGBolGxUVITEhJSkrLi4uFx8zODMtNygtLisBCgoKDg0OGhAQGy0lICUtL"},
                 {id: 2, name: 'Sacha'}
@@ -25,31 +20,40 @@ let state = {
                 {id: 2, message: "Coool"}
             ]
         },
-    sideBar: {
-        friends:[
-            {id:1, avatar:1},
-            {id:2, avatar:2}
-        ]
+        sideBar: {
+            friends:[
+                {id:1, avatar:1},
+                {id:2, avatar:2}
+            ]
+        },
+
     },
 
-    }
-    window.state=state;
-
-    export const addPost = ()=> {
-        let newPost = {
+    addPost() {
+        let newPost =  {
             id: 5,
-            message: state.profilePage.newPostText,
-            likesCount: 1
+                message: this._state.profilePage.newPostText,
+                likesCount: 1
         };
-        state.profilePage.posts.push(newPost);
-        state.profilePage.newPostText='';
-        rerenderEntireTree(state);
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText='';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText=newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber=observer;
+    },
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('state changed');
     }
-    export const updateNewPostText = (newText)=>{
-            state.profilePage.newPostText=newText;
-            rerenderEntireTree(state);
-    }
-    export const subscribe = (observer)=>{
 
-    }
-export default state;
+}
+
+export default store;
+window.store =store;
