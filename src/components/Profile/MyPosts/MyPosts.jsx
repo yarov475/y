c8849450fe2e2bdda251aa3386e557f01ac77afd
yayar <ya.yarochkin@yandex.ru>
2020-07-17 15:01:18 +0100

    actionCreator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,19 @@ import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
 
+let addPostActionCreator = () => {
+	return{
+		type: 'ADD-POST'
+	}
+
+}
+let updateNewPostTextActionCreeator = (text) => {
+	return{
+		type: 'UPDATE-NEW-POST-TEXT',newText: text
+	}
+}
+
+
 
 const MyPosts = (props) => {
 	let postElement =
@@ -10,12 +23,12 @@ const MyPosts = (props) => {
 	let newPostElement = React.createRef();
 
 	let addpost = () => {
-		props.dispatch({type: 'ADD-POST'});
+		props.dispatch(addPostActionCreator());
 	}
 
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-        let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text};
+        let action = updateNewPostTextActionCreeator(text);
         props.dispatch(action);
 
 	}
