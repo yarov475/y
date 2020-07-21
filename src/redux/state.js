e7d55355f1b003a262954bf00e37b950a8c53948
yayar <ya.yarochkin@yandex.ru>
2020-07-21 14:12:44 +0100

    41 reduser functions

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,8 +1,11 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = 'SEND_MESSAGE';
-
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 let store = {
     _state: {
         profilePage: {
@@ -60,28 +63,13 @@ let store = {
 
 
    dispatch(action) {
-        if(action.type === ADD_POST){
-            let newPost =  {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 1
-            };
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText='';
-            this._callSubscriber(this._state);
-        } else if (action.type === UPDATE_NEW_POST_TEXT){
-            this._state.profilePage.newPostText=action.newText;
-            this._callSubscriber(this._state);
+this._state.profilePage =  profileReducer(this._state.profilePage,action);
+this._state.dialogPage =  dialogsReducer(this._state.dialogPage,action);
+this._state.sideBar =  sidebarReducer(this._state.sideBar,action);
 
-        }else if (action.type === UPDATE_NEW_MESSAGE_BODY){
-            this._state.dialogPage.newMessageBody = action.body;
-            this._callSubscriber(this._state);
-        }else if (action.type === SEND_MESSAGE) {
-            let body = this._state.dialogPage.newMessageBody;
-            this._state.dialogPage.newMessageBody = ' ';
-            this._state.dialogPage.messages.push( {id: 2, message: body});
-            this._callSubscriber(this._state);
-        }
+this._callSubscriber(this._state);
+
+
    }
 }
 export const addPostActionCreator = () => ({type: ADD_POST})
