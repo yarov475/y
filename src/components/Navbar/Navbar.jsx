5698d628d09858c44acc25efdcae516e68873f1b
yayar <ya.yarochkin@yandex.ru>
2020-07-30 11:53:10 +0100

    55

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,22 +5,22 @@ import {NavLink} from "react-router-dom";
 
 
 const Navbar = (props) => {
-	return (
+    return (
 
-		<nav className={s.nav} >
+        <nav className={s.nav}>
 
-			<div> <NavLink to = '/profile' activeClassName= {s.active}> Profile </NavLink></div >
-			< div > <NavLink to ='dialog'activeClassName= {s.active}>MESAGES </NavLink> </div>
+            <div><NavLink to='/profile' activeClassName={s.active}> Profile </NavLink></div>
+            < div><NavLink to='dialog' activeClassName={s.active}>MESAGES </NavLink></div>
 
-			<div> <NavLink to = '/news' activeClassName= {s.active}> NEWS </NavLink> </div>
-			<div>< NavLink to = '/music' activeClassName= {s.active}> MUSIC</NavLink></div>
-			<div><NavLink to ='/settings'activeClassName= {s.active}>SETTINGS</NavLink></div>
+            <div><NavLink to='/news' activeClassName={s.active}> NEWS </NavLink></div>
+            <div>< NavLink to='/music' activeClassName={s.active}> MUSIC</NavLink></div>
+            <div><NavLink to='/settings' activeClassName={s.active}>SETTINGS</NavLink></div>
+            <div><NavLink to='/users' activeClassName={s.active}>USERS</NavLink></div>
 
-		</nav >
+        </nav>
 
 
-
-);
+    );
 
 }
 export default Navbar;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,29 +2,42 @@ import React from "react";
 import styles from './users.module.css';
 import *as axios from "axios";
 import userPhoto from './../../assets/images/userpng.png'
-let Users = (props) => {
-    if(props.users.length===0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
 
-            props.setUsers(response.data.items);
-
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
         });
+    }
 
-        }
 
-    return <div>
-        {
-      props.users.map(u=><div key={u.id}>
+    render() {
+        return <div>
+            <div>
+                <span>1</span>
+                 <span className={styles.selectedPage}>2</span>
+                 <span>3</span>
+                 <span>4</span>
+                 <span>5</span>
+
+            </div>
+
+            {
+                this.props.users.map(u => <div key={u.id}>
         <span>
             <div>
-                <img src={u.photos.small != null ? u.photos.small :userPhoto } className={styles.userPhoto}/>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
             </div>
             <div>
-                {u.followed? <button onClick={()=>{props.unfollow(u.id)}}> Unfollow</button>
-                    : <button onClick={()=>{props.follow(u.id)}}> follow</button>}
+                {u.followed ? <button onClick={() => {
+                        this.props.unfollow(u.id)
+                    }}> Unfollow</button>
+                    : <button onClick={() => {
+                        this.props.follow(u.id)
+                    }}> follow</button>}
               </div>
         </span>
-        <span>
+                    <span>
         <span>
             <div>{u.name}</div>
             <div>{u.status}</div>
@@ -34,11 +47,13 @@ let Users = (props) => {
             <div>{"u.location.city"}</div>
         </span>
       </span>
-          </div>)
-        }
-    </div>
-
-
+                </div>)
             }
+        </div>
+
+    }
+
+
+}
 
 export default Users;
