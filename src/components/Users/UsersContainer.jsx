754193e1268e7e6173b1cc3906126e81a049424b
yayar <ya.yarochkin@yandex.ru>
2020-08-17 14:53:35 +0100

    62 follow/1

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -17,9 +17,9 @@ class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-        {
-            withCredentials:true
-        })
+            {
+                withCredentials: true
+            })
 
             .then(response => {
                 this.props.toggleIsFetching(false);
@@ -33,7 +33,7 @@ class UsersContainer extends React.Component {
         this.props.setCurrentPage(pageNumber);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
             {
-                withCredentials:true
+                withCredentials: true
             })
             .then(response => {
                 this.props.toggleIsFetching(false);
